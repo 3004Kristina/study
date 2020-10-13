063d6c26f0c9f19311739b59d407fa2a562dd07d
sakhalin <73.vorobyev@gmail.com>
2020-10-13 21:46:45 +0300

    almost finished resume

---
+++ publish-proxy.js
@@ -0,0 +1,5 @@
+#! /usr/bin/env node
+
+const execSync = require('child_process').execSync;
+
+execSync('/bin/bash /home/sakhalin/bin/proxy 9100 kristina-resume');
--- src/resume/index.js
+++ src/resume/index.js
@@ -7,24 +7,42 @@ jQuery(function(){
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 2000,
         fade: true,
+        prevArrow: '<button type="button" class="slick-prev slick-arrow"><i class="fa fa-chevron-circle-left" aria-hidden="true"></i></button>',
+        nextArrow: '<button type="button" class="slick-next slick-arrow"><i class="fa fa-chevron-circle-right" aria-hidden="true"></button>',
         cssEase: 'linear'
     });
 });
 
 const header = document.querySelector('header'),
       header_btn = document.querySelector('.header_btn'),
-      about_me = document.querySelector('.about_me');
+      about_me = document.querySelector('.about_me'),
+      mobile_menu_btn = document.querySelector('.mobile_menu_btn'),
+      body = document.querySelector('body'),
+      mobile_menu_bg = document.querySelector('.mobile_menu_bg');
+
 
 window.addEventListener('scroll', () =>{
-    header.classList.toggle('scroll_bg', window.scrollY > 0);
+    header.classList.toggle('scroll_bg', window.scrollY > 100);
 });
 
-
-header_btn.addEventListener('click', () =>{
-    about_me.scrollIntoView({
-        behavior: 'smooth'
+if (header_btn && about_me){
+    header_btn.addEventListener('click', () =>{
+        about_me.scrollIntoView({
+            behavior: 'smooth'
+        });
     });
+}
+
+
+mobile_menu_btn.addEventListener('click', (e) =>{
+    e.preventDefault();
+    body.classList.toggle('mobile-menu-opened');
+
+});
+
+mobile_menu_bg.addEventListener('click', () =>{
+    body.classList.remove('mobile-menu-opened');
 });
 
